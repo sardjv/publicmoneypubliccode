7262b2ff4a4cab3d26e94d619217076020eb925c
Kevin Monk <kevin@sardjv.co.uk>
2021-04-29 14:16:07 +0100

    Boostrap v5 added

---
+++ app/frontend/js/bootstrap_js_files.js
@@ -0,0 +1,13 @@
+// inside app/frontend/js/bootstrap_js_files.js
+
+// import 'bootstrap/js/src/alert'
+// import 'bootstrap/js/src/button'
+// import 'bootstrap/js/src/carousel'
+import 'bootstrap/js/src/collapse'
+import 'bootstrap/js/src/dropdown'
+// import 'bootstrap/js/src/modal'
+// import 'bootstrap/js/src/popover'
+import 'bootstrap/js/src/scrollspy'
+// import 'bootstrap/js/src/tab'
+// import 'bootstrap/js/src/toast'
+// import 'bootstrap/js/src/tooltip'
